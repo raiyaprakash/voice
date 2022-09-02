d0b9084012a966d0ccf251324d3225ed58eafe27
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2022-09-02 11:42:08 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'Static';
+var filesToCache = [
+  //'index.html',
+  //'style.css',
+  'img/icon_512.png',
+  'img/icon_192.png'
+
+];
+
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
